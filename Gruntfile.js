e110f0bffc51cb96084eb81a8032835c6831330b
Matthew Smawfield <matthewsmawfield@gmail.com>
2017-10-16 13:44:45 +0100

    iframeResize.js for embedding

--- Gruntfile.js
+++ Gruntfile.js
@@ -73,7 +73,7 @@ module.exports = function (grunt) {
     // The actual grunt server settings
     connect: {
       options: {
-        port: 9001,
+        port: 9002,
         open: true,
         livereload: 35729,
         // Change this to '0.0.0.0' to access the server from outside
